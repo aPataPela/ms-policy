4fa9e419c2f411711ee18b6c312803d6e7ce78d8
Clau Vallejos <e_cvallejos@smu.cl>
2025-10-08 12:22:05 -0300

    feat: search policies base flow

--- src/app/controllers/policy-controller.ts
+++ src/app/controllers/policy-controller.ts
@@ -1,9 +1,16 @@
 import type { Request, Response } from "express";
 import { PolicyCreator } from '../../context/policy/application/policy-creator';
-
-export const policyController = (policyCreator: PolicyCreator) => ({
+import { PolicySearcher } from '../../context/policy/application/policy-searcher';
+export const policyController = (
+  policyCreator: PolicyCreator,
+  policySearcher: PolicySearcher,
+) => ({
   create: async (req: Request, res: Response) => {
     await policyCreator.run(req.body)
     res.status(201).send('inserted');
+  },
+  search: async (req: Request, res: Response) => {
+    const policy = await policySearcher.run();
+    res.status(200).send(policy);
   },
 });
--- src/context/policy/domain/contracts/PolicyRepository.ts
+++ src/context/policy/domain/contracts/PolicyRepository.ts
@@ -2,4 +2,5 @@ import { Policy } from '../class/Policy';
 
 export interface PolicyRepository {
   create(policy: Policy): Promise<void>;
+  search(): Promise<Policy[] | null>;
 }
